a175d1dc7cdf9d67be1ba4135770094bf91dc42a
Jean Paul <yjeanpaul@gmail.com~>
2019-10-02 13:06:02 -0500

    Version con las rutas listas para usar el analizador lexico

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+from analizadorLexico import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,18 +9,20 @@ def hello_world():
 @app.route('/programa',methods=['GET','POST'])
 def enviarPagina():
     if request.method == "POST":
-        print(request.form['programa'])
-        return request.form['programa']
+        tokens = request.form['programa']
+        print(tokens)
+        TokensRegex = obtenerTokensRegex()
+        codigo = open("prueba.txt",'r')
+        codigo = codigo.read()
+        lista = []
+        for i in re.finditer(TokensRegex, codigo):
+            lista.append((i, i.lastgroup))
+        tokensFalsos = [
+            ("variable", "a"),
+            ("llave", "{"),
+            ("llave", "}"),
+            ("palabrasReservadas", "IF")
+        ]
+        return render_template("tokens.html", tokens = lista)
     else:
         return render_template("prueba.html")
-
-@app.route('/tokens')
-def enviarTokens():
-    tokensFalsos = [
-        ("variable", "a"),
-        ("llave", "{"),
-        ("llave", "}"),
-        ("palabrasReservadas", "IF"),
-
-    ]
-    return render_template("tokens.html", tokens = tokensFalsos)
